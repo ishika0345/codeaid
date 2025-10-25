f22b78210a5366602ec0907994272d8c078b4f27
ishika0345 <ishikarathore101@gmail.com>
2025-10-25 18:24:11 +0530

    inngest error fixed

--- server/server/inngest/index.js
+++ server/server/inngest/index.js
@@ -1,13 +1,17 @@
 import { Inngest } from "inngest";
 import User from "../models/User.js";
+import connectDB from "../configs/db.js";
 
 // Create a client to send and receive events
 export const inngest = new Inngest({ id: "codeaid-app" });
+
+
 
 const syncUserCreation = inngest.createFunction(
     {id: 'sync-user-from-clerk'},
     {event:'clerk/user.created'},
     async({event})=>{
+        await connectDB();
         const{id,first_name,last_name,email_address,image_url}= event.data
         let username = email_address[0].email_address.split('@')[0]
 
@@ -30,6 +34,7 @@ const syncUserUpdation = inngest.createFunction(
     {id: 'update-user-from-clerk'},
     {event:'clerk/user.updated'},
     async({event})=>{
+        await connectDB();
         const{id,first_name,last_name,email_address,image_url}= event.data
         
         const updateUserData={
@@ -45,6 +50,7 @@ const syncUserDeletion = inngest.createFunction(
     {id: 'delete-user-from-clerk'},
     {event:'clerk/user.deleted'},
     async({event})=>{
+        await connectDB();
         const{id}= event.data
        await User.findByIdAndDelete(id)
 })
